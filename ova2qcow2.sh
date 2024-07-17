6c8799651feac44e5a9583bc6dac493f8a2a4c1e
Ted Pearson <1477390+tedpearson@users.noreply.github.com>
2024-07-17 15:37:42 -0400

    Initial commit

---
+++ ova2qcow2.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Usage: ova2qcow2 <file.ova>
+#
+# Required dependencies:
+# - qemu (brew install qemu)
+
+set -x
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: ova2utm.sh <file.ova>"
+	exit 1
+fi
+
+ova="$1"
+
+basedir=$(dirname "$ova")
+
+tmpdir=$(mktemp -d -t ova2utm.XXXXXXXX)
+tar xvzf "$ova" -C "$tmpdir"
+
+for i in ${tmpdir}/*.vmdk; do
+	name=$(basename -s .vmdk "$i")
+	qemu-img convert -O qcow2 "$i" "${basedir}/${name}.qcow2"
+done
+
+rm -r "$tmpdir"
